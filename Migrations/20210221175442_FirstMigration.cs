78d1a401585d259c9e7e8eab9d4ad7e729e4fd30
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-02-21 15:00:24 -0300

    correção da connectionString e primeira migration

---
+++ Migrations/20210221175442_FirstMigration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FatecMauaJobNewsletter.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobVacancies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    JobName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    JobDescription = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    JobArea = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: true),
+                    StartDateJobVacancy = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndDateJobVacancy = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    State = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: true),
+                    CompanyNumber = table.Column<string>(type: "nvarchar(6)", maxLength: 6, nullable: true),
+                    JobResponsible = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Salary = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FlagActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobVacancies", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobVacancies");
+        }
+    }
+}
