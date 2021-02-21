884b7ae66dc4b062514334daa035af6d117a6f60
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-02-21 11:08:48 -0300

    Inserido anotations, classe de mensagem de erro e bibliotecas no csproject

---
+++ Domains/Utils/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace FatecMauaJobNewsletter.Domains.Utils
+{
+    public static class ErrorMessages
+    {
+        public const string MaxLengthError = "{0} aceita no máximo {1} caracteres";
+    }
+}
