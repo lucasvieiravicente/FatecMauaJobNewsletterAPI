788f2eee159ead51132f07eda6be779bf57394e7
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-02-21 14:54:29 -0300

    inserido EFloggerFactory e configuração personalizada para coluna de salario

--- Domains/Contexts/DBContext.cs
+++ Domains/Contexts/DBContext.cs
@@ -1,13 +1,32 @@
 ﻿using FatecMauaJobNewsletter.Domains.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 
 namespace FatecMauaJobNewsletter.Domains.Contexts
 {
     public class DBContext : DbContext
     {
+        public static readonly LoggerFactory _loggerFactory = new LoggerFactory(new[] { new DebugLoggerProvider() });
+
         public DBContext(DbContextOptions<DBContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JobVacancy>()
+                                        .Property(p => p.Salary)
+                                        .HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLoggerFactory(_loggerFactory);
+            base.OnConfiguring(optionsBuilder);
         }
 
         DbSet<JobVacancy> JobVacancies { get; set; }
