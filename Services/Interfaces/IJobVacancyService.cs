e23b5ff3dcde4a8470442c1ad3172cb21b0bfa89
Lucas Vieira Vicente <lucasvieiravicente1@gmail.com>
2021-02-22 20:02:18 -0300

    inserido serviço referente as vagas

---
+++ Services/Interfaces/IJobVacancyService.cs
@@ -0,0 +1,19 @@
+﻿using FatecMauaJobNewsletter.Domains.Models;
+using FatecMauaJobNewsletter.Domains.Models.Request;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FatecMauaJobNewsletter.Services.Interfaces
+{
+    public interface IJobVacancyService
+    {
+        Task RegisterJob(JobVacancyRegisterRequest request);
+
+        Task UpdateJob(JobVacancyRegisterRequest request);
+
+        IEnumerable<JobVacancy> GetAllActive();
+
+        JobVacancy FindById(Guid id);
+    }
+}
---
+++ Services/JobVacancyService.cs
@@ -0,0 +1,44 @@
+﻿using FatecMauaJobNewsletter.Domains.Contexts.Interfaces;
+using FatecMauaJobNewsletter.Domains.Models;
+using FatecMauaJobNewsletter.Domains.Models.Request;
+using FatecMauaJobNewsletter.Services.Interfaces;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FatecMauaJobNewsletter.Services
+{
+    public class JobVacancyService : IJobVacancyService
+    {
+        private readonly IRepository<JobVacancy> _repository;
+        public JobVacancyService(IRepository<JobVacancy> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task RegisterJob(JobVacancyRegisterRequest request)
+        {
+            var job = request.Adapt<JobVacancy>();
+            await _repository.InsertAsync(job);
+        }
+
+        public async Task UpdateJob(JobVacancyRegisterRequest request)
+        {
+            JobVacancy job = _repository.FindById(request.Id);
+            request.Adapt(job);
+            await _repository.UpdateAsync(job);
+        }
+
+        public IEnumerable<JobVacancy> GetAllActive()
+        {
+            return _repository.GetAllActive();
+        }
+
+        public JobVacancy FindById(Guid id)
+        {
+            return _repository.FindById(id);
+        }
+
+    }
+}
